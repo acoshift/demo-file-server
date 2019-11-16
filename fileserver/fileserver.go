3ddd2e1adc609eb6db5a65d1cdce14b6f96fd5ca
acoshift <acoshift@gmail.com>
2019-11-16 13:50:04 +0700

    add test

---
+++ fileserver/fileserver.go
@@ -0,0 +1,5 @@
+package fileserver
+
+func Upload() {
+	panic("not implement")
+}
---
+++ fileserver/fileserver_test.go
@@ -0,0 +1,11 @@
+package fileserver_test
+
+import (
+	"testing"
+
+	"github.com/acoshift/demo-file-server/fileserver"
+)
+
+func TestUpload(t *testing.T) {
+	fileserver.Upload()
+}
